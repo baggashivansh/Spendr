8060919d9801ea239dd13911cead29809d5473b6
Shivansh Bagga <connect.shivanshbagga@gmail.com>
2026-02-17 12:27:45 +0530

    Defined the Structure

---
+++ src/main/java/com/spendr/SpendrApplication.java
@@ -0,0 +1,13 @@
+package com.spendr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spendr/SpendrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spendr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
